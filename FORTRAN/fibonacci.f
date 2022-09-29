2e9019d22d0d5885aebdeffd891319c8e4dfe30a
ZahidEC <100895632+ZahidEC@users.noreply.github.com>
2022-09-28 20:06:45 -0500

    Update fibonacci.f

--- FORTRAN/fibonacci.f
+++ FORTRAN/fibonacci.f
@@ -1,22 +1,23 @@
-         program fibonacci
-        ! Este programa calcula los numeros de la serie ! de fibonaccihasta nmax.
+        program fibonacci
+        ! Este programa calcula los numeros de la serie
+        ! de fibonacci hasta nmax.
         implicit none
-        integer i,nmax 17
+        integer i,nmax
         integer jnew,jold,aux
         ! Poner un limite.
         nmax = 10
-        ! Inicializarm(jnew,jold).
+        ! Inicializar (jnew,jold).
         jnew = 1; jold = 1
         ! Iniciar loop.
         print *
         do i=1,nmax
-        ! Imprimir elemento i de la serie.
-          print *, 'Elemento ',i,'de la serie de Fibonacci: ',jnew
-          print *
-          ! Calcular nuevo elemento de la serie.
-          aux = jnew
-          jnew = jnew + jold
-          jold = aux
+           ! Imprimir elemento i de la serie.
+           print *, 'Elemento ',i,' de la serie de Fibonacci: ', jnew
+           print *
+           ! Calcular nuevo elemento de la serie.
+           aux = jnew
+           jnew = jnew + jold
+           jold = aux
         end do
         pause
         end program fibonacci
